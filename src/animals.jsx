1aa1f723b0c48e0db40f73f7e21e616c9ef5ea25
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-10-02 15:20:08 -0700

    feat: rendering techniques

---
+++ src/animals.jsx
@@ -0,0 +1,49 @@
+function List(props) {
+  return (
+    <>
+      {!props.animals ? (
+        <div>Loading...</div>
+      ) : props.animals.length > 0 ? (
+        <ul>
+          {props.animals.map((animal) => {
+            return <li key={animal}>{animal}</li>;
+          })}
+        </ul>
+      ) : (
+        <div>There are no animals in the list!</div>
+      )}
+    </>
+  );
+}
+
+// or
+// function List(props) {
+//   return (
+//     <>
+//       {!props.animals && <div>Loading...</div>}
+//       {props.animals && props.animals.length > 0 && (
+//         <ul>
+//           {props.animals.map((animal) => {
+//             return <li key={animal}>{animal}</li>;
+//           })}
+//         </ul>
+//       )}
+//       {props.animals && props.animals.length === 0 && (
+//         <div>There are no animals in the list!</div>
+//       )}
+//     </>
+//   );
+// }
+
+function App2() {
+  const animals = [];
+
+  return (
+    <div>
+      <h1>Animals: </h1>
+      <List animals={animals} />
+    </div>
+  );
+}
+
+export default App2;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import App2 from "./animals.jsx";
 import { Greeting, Pizza, Test } from "./Greeting.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <App2 />
     <Greeting />
     <Pizza />
     <App />
