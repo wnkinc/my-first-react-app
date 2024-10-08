9177470ea101a82c0ce7aa27fce3c834625051b5
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-10-08 14:22:34 -0700

    How To Deal With Side Effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  return (
-    <>
-      <p>Hello World!</p>
-    </>
-  );
+  useEffect(() => {
+    const key = setInterval(() => {
+      setCounter((count) => count + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(key);
+    };
+  }, []);
+
+  return <p>{counter} seconds have passed.</p>;
 }
 
 export default App;
